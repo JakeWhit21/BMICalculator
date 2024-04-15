3da3e59f659fff610410f1be26d93716c1f391ed
Jake <jdwhitmore3@gmail.com>
2024-04-14 23:45:31 -0500

    Add project files.

---
+++ BMICalculator/Imports.cs
@@ -0,0 +1,5 @@
+global using BMICalculator;
+global using BMICalculator.Views;
+
+// Static
+global using static Microsoft.Maui.Graphics.Colors;
---
+++ BMICalculator/Views/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+namespace BMICalculator.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        private int _count = 0;
+        public int UserWeight = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            var version = typeof(MauiApp).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                ?.InformationalVersion;
+            VersionLabel.Text = $".NET MAUI ver. {version?[..version.IndexOf('+')]}";
+        }
+
+        private void CalculateBmi(object sender, EventArgs e)
+        {
+            double Weight = Convert.ToInt32(Weight_input.Text);
+            double Height = Convert.ToInt32(Height_input.Text);
+            double Output = (Weight / (Height * Height)) * 703;
+            OutputBmi.Text = $"Your BMI is {Output}";
+        }
+    }
+}
